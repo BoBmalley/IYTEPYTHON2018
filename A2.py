f13b0e57ca47506dba1b9e094d409d764ccb7fbc
Hâzim Alper Ata <44741542+BoBmalley@users.noreply.github.com>
2018-12-01 15:21:03 +0300

    Delete A2.py

--- A2.py
+++
@@ -1,8 +0,0 @@
-text_1=input('Bir yazı giriniz...')
-text_2=input('Kontrol edilecek yazı giriniz...')
-text_1=str.lower(text_1)
-text_2=str.lower(text_2)
-if text_2 in text_1:
-    print('İkinci yazdığınız ilk yazdığınızda GEÇİYOR...')
-else:
-    print('İkinci yazdığınız ilk yazdığınızda GEÇMİYOR...')
